36cf9c592acdd3a13d23f0516eb4a08f1b0b0adb
vanithavelu <vanithavelu1995@gmail.com>
2016-09-01 23:33:58 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class number 
+{
+public static void main(String args[])
+{
+int n=10;
+if(n==0)
+{
+System.out.println("given num is zero");
+}
+else if(n>0)
+{
+System.out.println("given num is positive");
+}
+else
+{
+System.out.println("given num is negative");
+}
+}
+}
