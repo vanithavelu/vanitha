433e3951d805197143f44f050cfdbd448a7ca8d0
vanithavelu <vanithavelu1995@gmail.com>
2016-09-04 00:51:54 +0530

    Create strcontains.java

---
+++ strcontains.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class strcontains
+{
+public static void main(String args[])
+{
+int i,j;
+String s;
+Scanner sc=new Scanner(System.in);
+s=sc.next();
+int n=s.length();
+if(s.contains("a")==true)
+{
+System.out.println("sucess");
+}
+else 
+{
+System.out.println("-1");
+}
+}
+}
+
+
